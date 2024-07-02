620b6b20aab49afad66b26c70f612f8e1fa823c1
oscar95011804161 <oscar_vanegas1@hotmail.com>
2024-07-02 17:12:48 -0500

    update javascript

---
+++ js/validar-inquilinos.js
@@ -0,0 +1,46 @@
+//video tutorial: https://youtu.be/36S19D6kZkc?si=Iftpx8dHxBIasCLS
+
+//Declaramos variables
+// de los id del documento html
+
+const nombre = document.getElementById("nombre");
+const celular = document.getElementById("celular");
+const email = document.getElementById("correo");
+const textarea = document.getElementById("exampleFormControlTextarea1");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+
+//validamos que nombre no tenga mas de 6 caracteres
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings =""
+    let entrar=false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ //creamos variable donde almacene expresion regular para validar email.
+    parrafo.innerHTML =""  //para vaciar la memoria cache
+    
+
+    if (nombre.value.length <6){
+        warnings+= `el nombre no es valido () <br>`
+        entrar="true"
+    }
+    if (celular.value.length <10){
+        warnings+= `el numero celular no es valido <br>`
+        entrar="true"
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El correo no es valido <br>`
+        entrar = true
+    }
+    if (textarea.value.length <6){
+        warnings+= `el comentario es muy corto <br>`
+        entrar="true"
+    }
+    
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "enviado"
+    }
+})
